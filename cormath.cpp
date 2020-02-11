d92886b9b9ebae6943b92034a23ecf877662de2f
こるぼー <48815255+zerokpr@users.noreply.github.com>
2020-02-11 12:25:45 +0900

    Update cormath.cpp

--- cormath.cpp
+++ cormath.cpp
@@ -1,13 +1,23 @@
 
 
-//最大公約数を求める
+
+// 最大公約数を求める
+// 計算量O(logn)
 template<class T>
 T gcd(T a, T b){
     if(b == 0) return a;
     return gcd(b, a%b);
 }
 
-//素数判定
+// 最小公倍数を求める
+// 計算量O(logn)
+template<class T>
+T lcm(T a, T b){
+    return a/gcd(a,b)*b;
+}
+
+// 素数判定
+// 計算量O(√n)
 template<class T>
 bool isPrime(T n){
     if(n < 2) return false;
@@ -15,15 +25,52 @@ bool isPrime(T n){
     return true;
 }
 
-//素因数分解
+// 素数列挙
+// nまでの素数をEratosthenesの篩で列挙する
+// 計算量O(nloglogn)
+template<class T>
+vector<T> prime_list(T n){
+    vector<T> ret;
+    vector<bool> notPrime(n+1,false);
+    notPrime[0] = notPrime[1] = true;
+    for(T i = 2; i <= n; ++i){
+        if(notPrime[i]) continue;
+        ret.push_back(i);
+        for(T j = i*2; j <= n; j += i) notPrime[j] = true;
+    }
+    return ret;
+}
 
 
-//関数名：pow
-//概要：modつき指数関数
-//第一引数a：指数の底
-//第二引数b：指数
-//第三引数mod：法となる値
-//戻り値：a^b(m mod)
+// 素因数分解
+// nを素因数分解してmapに記録
+// 計算量O(√n)
+template<class T>
+map<T,int> prime_factor(T n){
+    map<T,int> ret;
+    if(n <= 0){
+        cout << "Prime_factor Error" << "\n";
+        return ret;
+    }
+    T i = 2;
+    for(T i = 2; i*i <= n; ++i){
+        while(n%i== 0){
+            ++ret[i];
+            n /= i;
+        }
+    }
+    if(n != 1) ret[n]++;
+    return ret;
+}
+
+
+// 関数名：pow
+// 概要：modつき指数関数
+// 第一引数a：指数の底
+// 第二引数b：指数
+// 第三引数mod：法となる値
+// 戻り値：a^b(m mod)
+// 計算量O(log b)
 template<class T>
 T pow(T a, int b){
     if(b == 0) return 1;
@@ -51,6 +98,7 @@ T pow(long long a, long long b, T mod){
 //第二引数r：何個取り出すか
 //第三引数mod：法となる値
 //戻り値：nCr(m mod)
+// 計算量O(r), 第三引数まで指定するとO(rlogmod)
 template<class T>
 T comb(T n, T r){
     if(r > n) return 0;
@@ -62,6 +110,7 @@ T comb(T n, T r){
     }
     return ret;
 }
+
 template<class T>
 T comb(long long n,long long r,T mod){
     if(r > n) return 0;

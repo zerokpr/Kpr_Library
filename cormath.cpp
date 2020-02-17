be9d37502019dfc1ec8c9f9614a5366df5836951
こるぼー <48815255+zerokpr@users.noreply.github.com>
2020-02-17 21:48:38 +0900

    素数判定の修正
    素数判定のところが不十分だったので修正しました
    オーバーフローしづらくなりました

--- cormath.cpp
+++ cormath.cpp
@@ -19,7 +19,7 @@ T lcm(T a, T b){
 template<class T>
 bool isPrime(T n){
     if(n < 2) return false;
-    for(T i = 2; i*i < n; ++i) if(n % i == 0) return false;
+    for(long long i = 2; i*i <= n; ++i) if(n % i == 0) return false;
     return true;
 }
 

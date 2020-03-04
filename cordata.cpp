3660849796f36c6ab5cd52a48629e8160d8c2bb0
こるぼー <48815255+zerokpr@users.noreply.github.com>
2020-03-04 21:29:25 +0900

    Update cordata.cpp
    ・segment-treeを抽象化
    ・Union-Findに存在するグループの数を調べる関数を追加

--- cordata.cpp
+++ cordata.cpp
@@ -1,48 +1,66 @@
 
-//segment-tree
+// segment-tree
+// Tはモノイドの台集合
 template<typename T>
 class segment_tree{
 private:
-    vector<T> value; // ノードの値を持つ配列
-public:
+    using F = function<T(T,T)>;
     int leafnum;             // 葉の数
-    T m_val;                 // 最大値
+    const T m1;                 // 単位元
+    F f;
+    vector<T> seg; // ノードの値を持つ配列
     int bilower(int x){
-        int ans = 1;
-        while(ans < x) ans <<= 1;
-        return ans;
+        int ret = 1;
+        while(ret < x) ret <<= 1;
+        return ret;
     }
 
-    //constructor
-    //nodenumは配列の要素数、maxvalは最初に渡しておく最大値
-    segment_tree(int nodenum, T maxval){
-        leafnum = bilower(nodenum);
-        value = vector<T> (2*leafnum, maxval);
-        m_val = maxval;
-    }
-    void update(int i, T x){
-        // i 番目の葉の値を x に変える
-        i += leafnum - 1; // i 番目の葉のノード番号
-        value[i] = x;
-        while (i > 0) {
-            i = (i - 1) / 2; // ノード i の親ノードの番号に変える
-            value[i] = min(value[i * 2 + 1], value[i * 2 + 2]); // 左右の子の min を計算しなおす
-        }
-    }
-
-    //指定した区間の最小値を求める関数query
-    //引数a,bで指定した半開区間[a,b)の中の最小値を求める
-    //query(a,b,0,0,leafnum)として呼び出す、最期の引数に注意（leafnumはメンバ変数になっているのでそれを呼び出せばよい）
-    T query(int a, int b, int k, int l, int r){
+    T _query(int a, int b, int k, int l, int r){
         // [a, b)の区間に対するクエリについて
         // ノード k （区間 [l, r) 担当）が答える
-        if (r <= a || b <= l) return m_val; // 区間が被らない場合は 最大値 を返す
-        if (a <= l && r <= b) return value[k]; // ノード k の担当範囲がクエリ区間 [a, b)             
-        else{ //に完全に含まれる
-            T r1 = query(a, b, 2 * k + 1, l, (l + r) / 2); // 左の子に値を聞く
-            T r2 = query(a, b, 2 * k + 2, (l + r) / 2, r); // 右の子に値を聞く
-            return min(r1, r2); // 左右の子の値の min を取る
+        if (r <= a || b <= l) return m1; // 区間が被らない場合は 単位元 を返す
+        if (a <= l && r <= b) return seg[k]; // ノード k の担当範囲がクエリ区間 [a, b)
+        else{ // に完全に含まれる
+            T r1 = _query(a, b, 2 * k + 1, l, (l + r) / 2); // 左の子に値を聞く
+            T r2 = _query(a, b, 2 * k + 2, (l + r) / 2, r); // 右の子に値を聞く
+            return f(r1, r2); // 左右の子の値の f を取る
         }
+    }
+
+public:
+    
+    //constructor
+    //nodenumは配列の要素数、 はモノイドの単位元
+    segment_tree(int nodenum, const T m):m1(m){
+        leafnum = bilower(nodenum);
+        seg = vector<T> (2*leafnum, m);
+
+        // fの実装
+        f = [](T a, T b){
+            return a+b;
+        };
+    }
+
+    // i 番目の葉の値を x に変える
+    void update(int i, T x){    
+        i += leafnum - 1; // i 番目の葉のノード番号
+        seg[i] = x;
+        while (i > 0) {
+            i = (i - 1) / 2; // ノード i の親ノードの番号に変える
+            seg[i] = f(seg[i * 2 + 1], seg[i * 2 + 2]); // 左右の子の f を計算しなおす
+        }
+    }
+
+    //指定した区間の最小値を求める
+    //引数a,bで指定した半開区間[a,b)の中の最小値を求める
+    //_query(a,b,0,0,leafnum)として呼び出す、最期の引数に注意（leafnumはメンバ変数になっているのでそれを呼び出せばよい）
+    T query(int a, int b){
+        return _query(a, b, 0, 0, leafnum);
+    }
+    
+    // i番目の値を答える
+    T node(int i){
+        return seg[i+2*leafnum];
     }
 };
 
@@ -52,7 +70,7 @@ public:
 
 class union_find{
 private:
-    //根にはグループの要素数に―1をかけたもの、それ以外には根の値
+    //根にはグループの要素数に-1をかけたもの、それ以外には根の値
     //初期値は-1
     vector<int> uni;
 public:
@@ -88,5 +106,12 @@ public:
     //頂点aを含むグループの頂点数を調べる
     int unisize(int a){
         return -uni[root(a)];
+    }
+    //グループ数を調べる
+    int group_num(void){
+        int num = 0;
+        int s = uni.size();
+        for(int i = 0; i < s; i++) if(uni[i] < 0) num++;
+        return num;
     }
 };
